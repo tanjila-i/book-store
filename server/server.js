fc3d5e076fe8c571807c0e7010cf37a191dc77cb
Tanjila Islam <115866988+tanjila-i@users.noreply.github.com>
2026-05-05 11:56:08 +0600

    Update CORS configuration to include default URL 1

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || "https://book-store-frontend-p8i0.onrender.com", credentials: true }));
 app.use(cookieParser());
 
 // routes
