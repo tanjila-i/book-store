da5c81a7651946b261c74062e30347f867f24fcc
Tanjila Islam <115866988+tanjila-i@users.noreply.github.com>
2026-05-05 12:33:37 +0600

    Update CORS origin for production deployment 1

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://book-store-backend-weoc.onrender.com", credentials: true }));
 app.use(cookieParser());
 
 // routes
